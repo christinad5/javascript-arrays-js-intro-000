250dc947b0612fd1f0c1b38c0edeb71e58f1b719
Christina Dong <christinadongla@yahoo.com>
2019-01-11 00:52:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,4 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
-function removeElementFromBeginningOfArray(array, )
+function removeElementFromBeginningOfArray(array) {
+  array = array.slice(1);
+  return array;
+}
